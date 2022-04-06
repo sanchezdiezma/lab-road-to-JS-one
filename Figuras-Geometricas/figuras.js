93311bee8737d34583f0b0966c5e517f78feae3a
jmsd <sanchezdiezma@gmail.com>
2022-04-06 16:34:50 +0200

    Figuras Geometricas con variables

--- Figuras-Geometricas/figuras.js
+++ Figuras-Geometricas/figuras.js
@@ -1,5 +1,9 @@
 
-// Cuadrado
+// Utilizando datos estaticos a traves de variables 
+
+// Cuadrado_______________________________________________________________
+
+console.group("Cuadrado");
 
 const ladoCuadrado = 5;
 console.log('Los lados del cuadrado miden: ' + ladoCuadrado + ' cm');
@@ -8,4 +12,42 @@ const perimetroCuadrado = ladoCuadrado * 4;
 console.log('El perímetro del cuadrado es: ' + perimetroCuadrado + ' cm');
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log('El área del cuadrado es: ' + areaCuadrado + ' cm');
+console.log('El área del cuadrado es: ' + areaCuadrado + ' cm2');
+
+console.groupEnd();
+
+// Triangulo_______________________________________________________________
+
+console.group("Triangulo");
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+const areaTrianlgulo = (baseTriangulo * alturaTriangulo) / 2;
+
+console.log('Los lados del triángulo miden: ' + ladoTriangulo1 + ' cm y ' + ladoTriangulo2 + ' cm');
+console.log('La base del triángulo miden: ' + baseTriangulo + ' cm');
+console.log('La altura del triángulo miden: ' + alturaTriangulo + ' cm');
+console.log('El perimetro del triángulo es: ' + (ladoTriangulo1 + ladoTriangulo2 + baseTriangulo) + ' cm');
+console.log('El área del triángulo es: ' + areaTrianlgulo + ' cm2');
+
+console.groupEnd();
+
+// Circulo_______________________________________________________________
+
+console.group("Circulo");
+
+
+const radioCirculo = 4;
+const diametroCirculo = radioCirculo * 2;
+const pi = Math.PI;
+
+console.log('El radio del circulo es: ' + radioCirculo + ' cm');
+console.log('El diametro del circulo es: ' + diametroCirculo + ' cm');
+console.log('PI es: ' + pi);
+console.log('El perimetro del circulo es: ' + (pi * diametroCirculo) + ' cm');
+console.log('El área del circulo es: ' + (pi * radioCirculo * radioCirculo) + ' cm2');
+
+
+console.groupEnd();
